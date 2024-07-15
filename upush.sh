d61a282820bdb1aa17d79fd498259b08801e9767
Lucabevivino <lucabevivino13@gmail.com>
2024-07-15 20:31:26 +0200

    nuova prova

--- upush.sh
+++ upush.sh
@@ -1,6 +1,14 @@
-echo off
-echo "This upush.bat file is *not* safe - extremely rudimental"
+#!/bin/bash
+
+# Chiede all'utente di inserire il messaggio di commit
+read -p "Inserisci il messaggio di commit: " commit_message
+
+# Aggiunge tutti i file modificati allo staging area
 git add .
-git commit -m %1
-git pull
-git push
+
+# Esegue il commit con il messaggio fornito dall'utente
+git commit -m "$commit_message"
+
+# Esegue il pull e il push (puoi personalizzare i branch e il remote)
+git pull origin main
+git push origin main
